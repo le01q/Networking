243596ea333ff5f62a57a171456ce29313e457f7
leo1q <laraya1818@gmail.com>
2021-12-28 14:54:54 -0300

    Connection interpretation and important closure functions

---
+++ C/http-proxy/src/includes/connection.h
@@ -0,0 +1,13 @@
+struct connection_closure {
+    void (*on_read) (void* closure, char* buffer, int len);
+    void* on_read_closure;
+
+    void* (*on_close) (void* closure);
+    void* on_close_closure;
+
+    struct data_buffer_entry* write_bruffer;
+};
+
+extern void connection_write(struct epoll_event_handler* self, char* data, int len);
+extern void connection_close(struct epoll_event_handler* self);
+extern struct epoll_event_handler* create_connection(int connection_fd);
